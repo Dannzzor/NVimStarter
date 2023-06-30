a9d3d0b16b80ee9a34155bf02b522d496ad9586f
Danny Davenport <ddavenport420@gmail.com>
2023-06-29 21:34:48 -0400

    adds my cusomizations like neotree

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+-- not sure what to put here, just wanted to test it.
