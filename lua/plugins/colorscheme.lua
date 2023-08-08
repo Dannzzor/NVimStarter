2d50f6be1d218cc35df736c339115a483f1bbd2e
Danny Davenport <ddavenport420@gmail.com>
2023-08-08 10:01:19 -0400

    updates lazyvim

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -11,4 +11,14 @@ return {
   {
     "xiyaowong/transparent.nvim",
   },
+  {
+    -- highlight css colors
+    "brenoprata10/nvim-highlight-colors",
+  },
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      background_color = "#000000",
+    },
+  },
 }
