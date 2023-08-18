e5dfd90c766a7ed456b3eb5b3d672a4c7c5b53ee
Danny Davenport <ddavenport@futurecapital.com>
2023-08-18 15:49:46 -0400

    updates to re-add my shortcuts

--- init.lua
+++ init.lua
@@ -4,3 +4,5 @@ require("config.lazy")
 require("notify").setup({
   background_colour = "#000000",
 })
+
+require("nvim-highlight-colors").turnOn()
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,8 +19,8 @@ local function map(mode, lhs, rhs, opts)
 end
 
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>.", builtin.find_files, { desc = "Find files" })
-vim.keymap.set("n", "<leader>,", builtin.live_grep, { desc = "Grep" })
+vim.keymap.set("n", "<leader>,", builtin.find_files, { desc = "Find files" })
+vim.keymap.set("n", "<leader>.", builtin.live_grep, { desc = "Grep" })
 -- vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 -- vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 --
